ebc7f21b7cac387f639c88609300bcb7f94b1455
Greg <gfowler07@yahoo.com>
2012-04-21 14:06:44 -0400

    Now it all works! (I think...)

--- solver.asm
+++ solver.asm
@@ -1,3 +1,11 @@
+.data
+space:	.asciiz	" "
+newline:	.asciiz	"\n"
+no_soln:	.asciiz "No Soln"
+
+.text
+.globl main
+
 main:
 li $a0, 324 # allocate 81 words (9x9 array), so 324 bytes
 li $v0, 9
@@ -7,16 +15,133 @@ li $a0, 324
 li $v0, 9
 syscall
 move $s1, $v0 # $s1 is base address of array
+li $t0, 9
+sw $t0, 16($s1)
+sw $t0, 100($s1)
+sw $t0, 168($s1)
+sw $t0, 228($s1)
+sw $t0, 288($s1)
+li $t0, 7
+sw $t0, 24($s1)
+sw $t0, 176($s1)
+sw $t0, 200($s1)
+sw $t0, 220($s1)
+sw $t0, 280($s1)
+sw $t0, 304($s1)
+li $t0, 5
+sw $t0, 32($s1)
+sw $t0, 92($s1)
+sw $t0, 172($s1)
+sw $t0, 224($s1)
+sw $t0, 268($s1)
+li $t0, 6
+sw $t0, 40($s1)
+sw $t0, 88($s1)
+sw $t0, 132($s1)
+sw $t0, 164($s1)
+sw $t0, 188($s1)
+li $t0, 3
+sw $t0, 52($s1)
+sw $t0, 96($s1)
+sw $t0, 120($s1)
+sw $t0, 144($s1)
+sw $t0, 236($s1)
+sw $t0, 256($s1)
+li $t0, 2
+sw $t0, 64($s1)
+sw $t0, 156($s1)
+sw $t0, 232($s1)
+li $t0, 4
+sw $t0, 76($s1)
+sw $t0, 244($s1)
+sw $t0, 296($s1)
+li $t0, 1
+sw $t0, 152($s1)
+sw $t0, 196($s1)
+li $t0, 8
+sw $t0, 84($s1)
+sw $t0, 124($s1)
+sw $t0, 148($s1)
+move $t0, $s1
+move $t1, $s0
+li $t2, 324
+add $t2, $t2, $s1
+populate_output_array:
+lw $t3, 0($t0)
+sw $t3, 0($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+bne $t0, $t2, populate_output_array
+move $t0, $s1
+li $t4, 36
+li $t5, 0
+print_orig_array:
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+la $a0, space
+li $v0, 4
+syscall
+addi $t0, $t0, 4
+addi $t5, $t5, 4
+div $t5, $t4
+mfhi $t6
+bne $t6, $zero, test_print_orig
+la $a0, newline
+li $v0, 4
+syscall
+test_print_orig:
+bne $t0, $t2, print_orig_array
+li $a0, 0 # preparing to call backtrack(0,0)
+li $a1, 0 # see previous line
+addi $sp, $sp, -8
+sw $a0, 4($sp)
+sw $a1, 0($sp)
+jal backtrack
+lw $a1, 0($sp)
+lw $a0, 4($sp)
+addi $sp, $sp, 8
+beq $v0, $zero, print_no_soln
+la $a0, newline
+li $v0, 4
+syscall
+move $t0, $s0
+move $t9, $zero
+li $t4, 36
+li $t2, 324
+print_solved_array:
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+la $a0, space
+li $v0, 4
+syscall
+addi $t0, $t0, 4
+addi $t9, $t9, 4
+div $t9, $t4
+mfhi $t5
+bne $t5, $zero, no_print_newline
+la $a0, newline
+li $v0, 4
+syscall
+no_print_newline:
+bne $t9, $t2, print_solved_array
+j end
+print_no_soln:
+la $a0, no_soln
+li $v0, 4
+syscall
+j end
 
 # $t0 = temp, $t1 = result of temp calculation (orig. j, but that's not used), $t2 = bunch of stuff in forming outputArray[x][y]
-# $t3 = 9, $t4 = 1, $t5 = 8, $t6 = 10
+# $t3 = 9, $t4 = 1, $t5 = 8, $t9 = 10 (changed $t6 to use for something else below)
 # $a0 = x, $a1 = y, $a2 = i
 # $t7 = temporarily used to store value of x upon function call, $t8 = temporarily used to store value of y upon function call
 backtrack:
 li $t3, 9
 li $t4, 1
 li $t5, 8
-li $t6, 10
+li $t7, 10
 mult $a0, $t3
 mflo $t2 # $t2 = x*9
 add $t2, $t2, $a1 # $t2 = x*9 + y
@@ -24,9 +149,12 @@ sll $t2, $t2, 2 # $t2 = (x*9 + y)*4
 add $t2, $s0, $t2 # now $t2 contains address of outputArray[x][y]
 lw $t2, 0($t2)
 bne $t2, $zero, arr_not_zero # skip to else after if(outputArray[x][y] == 0)
-addi $a2, $zero, 1 # i = 1
+move $a2, $zero # i = 0 (will be changed to 1, which is its actual initial value, right after entering i_loop)
+li $t9, 10
 i_loop:
 addi $a2, $a2, 1
+slt $s5, $a2, $t9 # if i < 10, $s5 is set to 1
+beq $s5, $zero, end_i_loop
 addi $sp, $sp, -44
 sw $ra, 40($sp)
 sw $a2, 36($sp)
@@ -55,11 +183,13 @@ addi $sp, $sp, 44
 move $t0, $v0 # move value returned from input_value to temp
 slt $t1, $zero, $t0 # if 0 < temp (i.e., temp > 0), $t1 = 1; otherwise, $t1 = 0
 beq $t1, $zero, i_loop
-mflo $t2 # $t2 = x*9 (this should still be in lo register w/o needing another multiply)
+mult $a0, $t3
+mflo $t2 # $t2 = x*9
 add $t2, $t2, $a1 # $t2 = x*9 + y
 sll $t2, $t2, 2 # $t2 = (x*9 + y)*4
 add $t2, $s0, $t2 # now $t2 contains address of outputArray[x][y]
 sw $t0, 0($t2) # store temp in outputArray[x][y] (outputArray[x][y] = temp)
+
 move $t7, $a0
 move $t8, $a1
 bne $a0, $t5, output_zero_else # if x != 8, can't be true that x == 8 && y == 8 (also don't need to test it twice)
@@ -67,8 +197,6 @@ bne $a1, $t5, output_zero_else_if # same for y != 8; using deMorgan's law b/c !(
 li $v0, 1
 jr $ra
 output_zero_else_if:
-move $a0, $zero # x = 0 for function call
-addi $a1, $a1, 1 # y = y+1 for function call
 # call function
 addi $sp, $sp, -44
 sw $ra, 40($sp)
@@ -82,6 +210,8 @@ sw $t3, 12($sp)
 sw $t2, 8($sp)
 sw $t1, 4($sp)
 sw $t0, 0($sp)
+move $a0, $zero # x = 0 for function call (not really changing x in this call, which is why this is placed after $a0 is still in memory)
+addi $a1, $a1, 1 # y = y+1 for function call (see above)
 jal backtrack
 lw $t0, 0($sp)
 lw $t1, 4($sp)
@@ -98,7 +228,6 @@ addi $sp, $sp, 44
 beq $v0, $zero, i_loop
 jr $ra # if $v0 != 0, then it's 1 so it already contains what we need to return
 output_zero_else:
-addi $a0, $a0, 1 # x = x+1 for function call
 # call function
 addi $sp, $sp, -44
 sw $ra, 40($sp)
@@ -112,6 +241,7 @@ sw $t3, 12($sp)
 sw $t2, 8($sp)
 sw $t1, 4($sp)
 sw $t0, 0($sp)
+addi $a0, $a0, 1 # x = x+1 for function call (again, not actually changing x)
 jal backtrack
 lw $t0, 0($sp)
 lw $t1, 4($sp)
@@ -129,16 +259,21 @@ beq $v0, $zero, i_loop
 jr $ra
 move $a0, $t7 # can put these two instructions here b/c if we don't reach this point, we're exiting method so they're not needed
 move $a1, $t8
-bne $a2, $t6, i_loop
 # don't see why it would be necessary to test if i == 10, as that seems evident at this point
-lw $t2, 0($t2) # $t2 should still be address of outputArray[x][y], so just load it
+
+end_i_loop:
 mult $a0, $t3
-mflo $t1
+mflo $t2 # $t2 = x*9
+add $t2, $t2, $a1 # $t2 = x*9 + y
+sll $t2, $t2, 2 # $t2 = (x*9 + y)*4
+add $t2, $s0, $t2 # now $t2 contains address of outputArray[x][y]
+lw $s3, 0($t2)
+mflo $t1 # still using product of $a0 and $t3, so don't need to multiply again
 add $t1, $t1, $a1
 sll $t1, $t1, 2
 add $t1, $s1, $t1 # now $t1 contains address of array[x][y]
 lw $t1, 0($t1)
-beq $t1, $t2, arrays_match
+beq $t1, $s3, arrays_match
 sw $zero, 0($t2) # outputArray[x][y] = 0
 arrays_match:
 move $v0, $zero
@@ -151,8 +286,6 @@ bne $a1, $t5, output_not_zero_else_if # same for y != 8; using deMorgan's law b/
 li $v0, 1
 jr $ra
 output_not_zero_else_if:
-move $a0, $zero # x = 0 for function call
-addi $a1, $a1, 1 # y = y+1 for function call
 # call function
 addi $sp, $sp, -44
 sw $ra, 40($sp)
@@ -166,6 +299,8 @@ sw $t3, 12($sp)
 sw $t2, 8($sp)
 sw $t1, 4($sp)
 sw $t0, 0($sp)
+move $a0, $zero # x = 0 for function call (not really changing x in this call, which is why this is placed after $a0 is still in memory)
+addi $a1, $a1, 1 # y = y+1 for function call (see above)
 jal backtrack
 lw $t0, 0($sp)
 lw $t1, 4($sp)
@@ -181,7 +316,6 @@ lw $ra, 40($sp)
 addi $sp, $sp, 44
 jr $ra # if $v0 == 0, I think we need to return 0, based on what I read at http://stackoverflow.com/questions/1610030/why-can-you-return-from-a-non-void-function-without-returning-a-value-without-pr
 output_not_zero_else:
-addi $a0, $a0, 1 # x = x+1 for function call
 # call function
 addi $sp, $sp, -44
 sw $ra, 40($sp)
@@ -195,6 +329,7 @@ sw $t3, 12($sp)
 sw $t2, 8($sp)
 sw $t1, 4($sp)
 sw $t0, 0($sp)
+addi $a0, $a0, 1 # x = x+1 for function call (again, not actually changing x)
 jal backtrack
 lw $t0, 0($sp)
 lw $t1, 4($sp)
@@ -213,6 +348,7 @@ jr $ra
 
 input_value:
 li $t0, 9
+li $t4, 6
 move $t1, $zero # i = 0
 horiz_and_vert:
 mult $t1, $t0
@@ -253,8 +389,9 @@ addi $t2, $t2, 1
 bne $t2, $t0, j_x_less_3_y_less_3
 addi $t1, $t1, 1
 bne $t1, $t0, i_x_less_3_y_less_3
+move $v0, $a2 # want to return value
+jr $ra
 x_less_3_y_3_to_5:
-li $t4, 6
 slt $t1, $a1, $t4 # if y < 6, $t1 is set to 1
 beq $t1, $zero, x_less_3_y_6_to_8
 move $t1, $zero # i = 0
@@ -272,6 +409,8 @@ addi $t2, $t2, 1
 bne $t2, $t4, j_x_less_3_y_3_to_5
 addi $t1, $t1, 1
 bne $t1, $t0, i_x_less_3_y_3_to_5
+move $v0, $a2 # want to return value
+jr $ra
 x_less_3_y_6_to_8:
 move $t1, $zero # i = 0
 i_x_less_3_y_6_to_8:
@@ -288,8 +427,8 @@ addi $t2, $t2, 1
 bne $t2, $t5, j_x_less_3_y_6_to_8
 addi $t1, $t1, 1
 bne $t1, $t0, i_x_less_3_y_6_to_8
-j input_value_ret
-
+move $v0, $a2 # want to return value
+jr $ra
 x_3_to_5:
 slt $t1, $a0, $t4 # else if x < 6, $t1 is set to 1
 beq $t1, $zero, x_6_to_8
@@ -310,6 +449,8 @@ addi $t2, $t2, 1
 bne $t2, $t0, j_x_3_to_5_y_less_3
 addi $t1, $t1, 1
 bne $t1, $t4, i_x_3_to_5_y_less_3
+move $v0, $a2 # want to return value
+jr $ra
 x_3_to_5_y_3_to_5:
 slt $t1, $a1, $t4 # if y < 6, $t1 is set to 1
 beq $t1, $zero, x_3_to_5_y_6_to_8
@@ -328,6 +469,8 @@ addi $t2, $t2, 1
 bne $t2, $t4, j_x_3_to_5_y_3_to_5
 addi $t1, $t1, 1
 bne $t1, $t4, i_x_3_to_5_y_3_to_5
+move $v0, $a2 # want to return value
+jr $ra
 x_3_to_5_y_6_to_8:
 move $t1, $t0 # i = 3
 i_x_3_to_5_y_6_to_8:
@@ -344,8 +487,8 @@ addi $t2, $t2, 1
 bne $t2, $t5, j_x_3_to_5_y_6_to_8
 addi $t1, $t1, 1
 bne $t1, $t4, i_x_3_to_5_y_6_to_8
-j input_value_ret
-
+move $v0, $a2 # want to return value
+jr $ra
 x_6_to_8:
 slt $t1, $a1, $t0 # if y < 3, $t1 is set to 1
 beq $t1, $zero, x_6_to_8_y_3_to_5
@@ -364,6 +507,8 @@ addi $t2, $t2, 1
 bne $t2, $t0, j_x_6_to_8_y_less_3
 addi $t1, $t1, 1
 bne $t1, $t5, i_x_6_to_8_y_less_3
+move $v0, $a2 # want to return value
+jr $ra
 x_6_to_8_y_3_to_5:
 slt $t1, $a1, $t4 # if y < 6, $t1 is set to 1
 beq $t1, $zero, x_6_to_8_y_6_to_8
@@ -382,6 +527,8 @@ addi $t2, $t2, 1
 bne $t2, $t4, j_x_6_to_8_y_3_to_5
 addi $t1, $t1, 1
 bne $t1, $t5, i_x_6_to_8_y_3_to_5
+move $v0, $a2 # want to return value
+jr $ra
 x_6_to_8_y_6_to_8:
 move $t1, $t4 # i = 6
 i_x_6_to_8_y_6_to_8:
@@ -398,12 +545,14 @@ addi $t2, $t2, 1
 bne $t2, $t5, j_x_6_to_8_y_6_to_8
 addi $t1, $t1, 1
 bne $t1, $t5, i_x_6_to_8_y_6_to_8
-j input_value_ret
+move $v0, $a2 # want to return value
+jr $ra
 
 set_to_zero:
 move $v0, $zero # return 0
 jr $ra
 
-input_value_ret:
-move $v0, $a2 # return value
-jr $ra
+end:
+la $a0, newline
+li $v0, 4
+syscall
